94abf0715f70133dce6f1bca2138608c0589804d
Alejandro Rojas <4lejandr0rojas@protonmail.com>
2023-01-05 17:49:49 -0600

    Sec 4: Spring Cloud Gateway - p36. Modificando el request en el filtro pre.

--- src/main/java/com/formacionbdi/springboot/app/gateway/filters/EjemploGlobalFilter.java
+++ src/main/java/com/formacionbdi/springboot/app/gateway/filters/EjemploGlobalFilter.java
@@ -2,6 +2,7 @@ package com.formacionbdi.springboot.app.gateway.filters;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -10,18 +11,29 @@ import lombok.extern.slf4j.Slf4j;
 
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Component
 @Slf4j
-public class EjemploGlobalFilter implements GlobalFilter{
+public class EjemploGlobalFilter implements GlobalFilter, Ordered {
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		log.info("Ejecutando filtro pre");
+		exchange.getRequest().mutate().headers(header -> header.add("token", "12345"));
+
 		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
 			log.info("Ejecutando filtro post");
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
+				exchange.getResponse().getHeaders().add("token", valor);
+			});
 			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
 			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
 	}
 
+	@Override
+	public int getOrder() {
+		return -1;
+	}
 }
